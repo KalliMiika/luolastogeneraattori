ac83b4045eac3c79ebe2d45762edf76409b73410
KalliMiika <miika.kallioinen@helsinki.fi>
2020-01-31 23:47:20 +0200

    checkstyle päivitys

--- luolastogeneraattori/src/main/java/luolastogeneraattori/objects/Point.java
+++ luolastogeneraattori/src/main/java/luolastogeneraattori/objects/Point.java
@@ -14,10 +14,12 @@ public class Point {
         this.x = 0;
         this.y = 0;
     }
+
     /**
      * Point -luokan konstruktori
-     * @param x     Point olion x koordinaatti
-     * @param y     Point olion y koordinaatti
+     *
+     * @param x Point olion x koordinaatti
+     * @param y Point olion y koordinaatti
      */
     public Point(int x, int y) {
         this.x = x;
@@ -25,7 +27,7 @@ public class Point {
     }
 
     /**
-     * 
+     *
      * @return Palauttaa Point -olion X-koordinaatin
      */
     public int getX() {
@@ -33,7 +35,7 @@ public class Point {
     }
 
     /**
-     * 
+     *
      * @return Palauttaa Point -olion Y-koordinaatin
      */
     public int getY() {
@@ -42,55 +44,59 @@ public class Point {
 
     /**
      * Asettaa Point -olion X-koordinaatin
-     * @param x     asetettava x-koordinaatti
+     *
+     * @param x asetettava x-koordinaatti
      */
     public void setX(int x) {
         this.x = x;
     }
-    
+
     /**
      * Asettaa Point -olion Y-koordinaatin
-     * @param y     asetettava y-koordinaatti
+     *
+     * @param y asetettava y-koordinaatti
      */
     public void setY(int y) {
         this.y = y;
     }
 
     /**
-     * Lisää parametrina annetun Point-olion koordinaatit
-     * tämän Point-olion koordinaatteihin.
-     * @param point     Lisättävä Point-olio
+     * Lisää parametrina annetun Point-olion koordinaatit tämän Point-olion
+     * koordinaatteihin.
+     *
+     * @param point Lisättävä Point-olio
      */
     public void add(Point point) {
         this.x += point.getX();
         this.y += point.getY();
     }
-    
+
     /**
-     * Vähentää parametrina annetun Point-olion koordinaatit
-     * tämän Point-olion koordinaateista.
-     * @param point     Vähennettävä Point-olio
+     * Vähentää parametrina annetun Point-olion koordinaatit tämän Point-olion
+     * koordinaateista.
+     *
+     * @param point Vähennettävä Point-olio
      */
     public void subtract(Point point) {
         this.x -= point.getX();
         this.y -= point.getY();
     }
-    
-    
+
     /**
      * Kloonaa tämän Point-Olion
-     * @return  Point   Kopio tästä Point-Oliosta
+     *
+     * @return Point Kopio tästä Point-Oliosta
      */
     public Point clone() {
         return new Point(this.x, this.y);
     }
-    
+
     /**
-     * Tarkistaa voidaanko parametrina annettu Point-olio
-     * lisätä tähän Point-olioon.
-     * Jos voidaan, lisätään se tähän Point-olioon ja palautetaan false
-     * Jos ei voida, lisäystä ei tehdä ja palautetaan true
-     * @param point     Tarkistettava ja Lisättävä Point-olio
+     * Tarkistaa voidaanko parametrina annettu Point-olio lisätä tähän
+     * Point-olioon. Jos voidaan, lisätään se tähän Point-olioon ja palautetaan
+     * false Jos ei voida, lisäystä ei tehdä ja palautetaan true
+     *
+     * @param point Tarkistettava ja Lisättävä Point-olio
      * @return true jos lisäys ei onnistu, false muutoin
      */
     public boolean checkAndAdd(Point point) {
@@ -100,60 +106,59 @@ public class Point {
         add(point);
         return false;
     }
-    
+
     /**
-     * Tarkistaa voidaanko parametrina annettu Point-olio
-     * lisätä tähän Point-olioon.
-     * Jos voidaan, palautetaan false
-     * Jos ei voida, palautetaan true
-     * @param point     Tarkistettava Point-olio
+     * Tarkistaa voidaanko parametrina annettu Point-olio lisätä tähän
+     * Point-olioon. Jos voidaan, palautetaan false Jos ei voida, palautetaan
+     * true
+     *
+     * @param point Tarkistettava Point-olio
      * @return true jos lisäys ei onnistu, false muutoin
      */
     public boolean check(Point point) {
         int x = this.x + point.getX();
-        if (x < 1 || x >= Cave.WIDTH-1) {
+        if (x < 1 || x >= Cave.WIDTH - 1) {
             return true;
         }
         int y = this.y + point.getY();
-        if (y < 1 || y >= Cave.HEIGHT-1) {
+        if (y < 1 || y >= Cave.HEIGHT - 1) {
             return true;
         }
         return false;
     }
 
     /**
-     * Luodaan ja palautetaan uusi satunnaiseen lokaatioon
-     * luotu Piste-olio
-     * Piste ei kuitenkaan saa osua ruudun reunoihin, joten
-     * vähennetään satunnaisuuden maksimiarvoista 2 ja lisätään
-     * minimiarvoon 1
+     * Luodaan ja palautetaan uusi satunnaiseen lokaatioon luotu Piste-olio
+     * Piste ei kuitenkaan saa osua ruudun reunoihin, joten vähennetään
+     * satunnaisuuden maksimiarvoista 2 ja lisätään minimiarvoon 1
+     *
      * @return randomgeneroitu point-olio
      */
     public static Point randomPoint() {
         Random rnd = new Random();
-        int x = rnd.nextInt(Cave.WIDTH-2)+1;
-        int y = rnd.nextInt(Cave.HEIGHT-2)+1;
+        int x = rnd.nextInt(Cave.WIDTH - 2) + 1;
+        int y = rnd.nextInt(Cave.HEIGHT - 2) + 1;
         return new Point(x, y);
     }
 
     /**
-     * Palauttaa String-tyyppisen kuvauksen Point-oliosta.
-     * Muodossa "(x, y)"
+     * Palauttaa String-tyyppisen kuvauksen Point-oliosta. Muodossa "(x, y)"
+     *
      * @return String-tyyppinen kuvaus Point-oliosta
      */
     public String toString() {
         return "(" + this.x + ", " + this.y + ")";
     }
-    
+
     /**
-     * Vertaa parametrina annettua Point-Oliota itseensä
-     * Jos olioiden parametrit täsmäävät, palautetaan true
-     * muutoin false
-     * @param point     Point   Verrattava Point-Olio
-     * @return          boolean true jos sama, false muutoin
+     * Vertaa parametrina annettua Point-Oliota itseensä Jos olioiden parametrit
+     * täsmäävät, palautetaan true muutoin false
+     *
+     * @param point Point Verrattava Point-Olio
+     * @return boolean true jos sama, false muutoin
      */
     public boolean equals(Point point) {
-        if(this.x == point.getX() && this.y == point.y) {
+        if (this.x == point.getX() && this.y == point.y) {
             return true;
         }
         return false;
